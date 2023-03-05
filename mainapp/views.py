24b4b157fa0279456bd4a48b0fa5b2595d119017
yoe herrera <yoeherrera@gmail.com>
2023-03-05 14:46:24 -0500

    Completed module

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.http import Http404
+from .models import BlogPost
 
 from datetime import datetime
 ALL_POSTS = [
@@ -23,12 +24,12 @@ ALL_POSTS = [
 ]
 
 def index(request):
-    posts = ALL_POSTS
+    posts =  BlogPost.objects.all()
     return render(request, 'mainapp/index.html', {'posts':posts})
 
 def post(request, id):
     try:
-        post = ALL_POSTS[id]
+        post = get_object_or_404(BlogPost, pk=id) 
     except IndexError:
         raise Http404("Post does not exist")
     return render(request, 'mainapp/post.html', {'object': post})
--- users/models.py
+++ users/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.urls import reverse
 
 class User(AbstractUser):
     pass
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200,unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="posts",on_delete=models.CASCADE)
+    body = models.TextField()
+    postdate = models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post",args=[str(self.id)]) 
 
 
