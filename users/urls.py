9a7ab990beff83758f666a4ad8e057f3407af75f
yoe herrera <yoeherrera@gmail.com>
2023-03-05 14:33:30 -0500

    Completed module

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import views, auth_views
-from django.urls import views
+from django.contrib.auth import views as auth_views
+from django.urls import path
 from . import views
 
 urlpatterns = [
